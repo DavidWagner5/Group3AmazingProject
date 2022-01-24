80aa62458fe02a9c4793d02db7c40710b2d0bb8a
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-01-24 17:23:00 -0500

    Created new project under the name tariq 002

---
+++ src/Hello/HelloTeam.java
@@ -0,0 +1,8 @@
+package Hello;
+
+public class HelloTeam {
+
+    // Sending request.
+
+    // my update message
+}
--- src/us100/Tariq001.java
+++ src/us100/Tariq001.java
@@ -5,5 +5,7 @@ public class Tariq001 {
     // im adding my first commit
     public static void main(String[] args) {
 
+
+
     }
 }
